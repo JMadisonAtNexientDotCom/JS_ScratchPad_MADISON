0fdf3e9fded632b8dea383582c901ce283bdb304
John Mark Isaac Madison <JMadison@Nexient.com>
2015-10-26 11:10:30 -0400

    Making sure changes in NinjaTrialShow show up here.

--- ByProject/NexientLoaders/require_js_test/include_loader.js
+++ ByProject/NexientLoaders/require_js_test/include_loader.js
@@ -4,12 +4,14 @@
 // except for 'app' ones, which are in a sibling
 // directory.
 requirejs.config({
-    baseUrl: 'lib',
-    paths: {
-        app: '../app'
-    }
+    baseUrl: 'madison_lib',
+		
+		//paths not needed:
+    //paths: {
+    //    madison_app: '../madison_app'
+    //}
 });
 
 // Start loading the main app file. Put all of
 // your application logic in there.
-requirejs(['app/permu']);
+requirejs(['madison_app/NexFracLoader']);
